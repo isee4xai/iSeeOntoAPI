d9f388a904e8ea5c277b7dec837003f7674900c5
Marta C.M <martcaro@ucm.es>
2022-11-22 18:56:36 +0100

    Explainer insertion included

--- src/routes/queries.js
+++ src/routes/queries.js
@@ -22,17 +22,13 @@ router.get('/UserIntent', queryctrl.getUserIntent);
 router.get('/UserDomain', queryctrl.getUserDomain);
 router.get('/KnowledgeLevel', queryctrl.getKnowledgeLevel);
 // router.get('/TechnicalFacilities', queryctrl.getTechnicalFacilities);
-
-
-// Cockpit Required APIs
-router.get('/cockpit/Usecases', queryctrl.getCockpitUsecases);
-router.get('/cockpit/ExplainerFields', queryctrl.getExplainerFields);
-router.get('/cockpit/DialogFields', queryctrl.getDialogFields);
+router.get('/getExplainers', queryctrl.getExplainer);
 
 
 // ADMIN ONLY APIs
 // Dump the existing ontology
 router.post('/dump', queryctrl.dump);
 router.post('/anyQueryAdmin', queryctrl.anyQueryAdmin);
+router.post('/insertExplainer', queryctrl.insertExplainer); // adding a new explainer into the ontology
 
 module.exports = router;
