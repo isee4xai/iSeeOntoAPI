3d39a615231b0a180c6eda53b6c474f3cf2e63e5
anjanaw <shyameniw@gmail.com>
2023-10-12 18:30:30 +0100

    test: services

--- src/controllers/reuse.js
+++ src/controllers/reuse.js
@@ -4,7 +4,7 @@ const UtilService = require("../services/util");
 module.exports.reuseSupport = async (req, res) => {
     try {
         const result = await UtilService.reuseSupport();
-        return result;
+        res.status(200).json(result); 
     } catch (error) {
         res.status(500).json({ message: error });
     }
